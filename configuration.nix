a3352b6be5879d433dd12d91f6a765cb3cf2a12c
carter <smit3cg@mail.uc.edu>
2025-06-01 12:50:51 -0400

    added things

--- configuration.nix
+++ configuration.nix
@@ -104,11 +104,12 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-	  	vim 
+	  	neovim 
 		lf
 		git
 		gh
 		librewolf
+git-credential-manager
 #  wget
   ];
 
